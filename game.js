9d167816f3d919cce8d9e247af64e9d15b9371b0
mohanamisra <iammohanamisra@gmail.com>
2021-12-29 20:10:20 +0530

    Updated the js file

--- game.js
+++ game.js
@@ -1,10 +1,28 @@
+let boy = document.getElementById("boy");
+let robot = document.getElementById("robot");
+let images = document.body.getElementsByTagName("img");
+let boyImage = images[3];
+let robotImage = images[4];
+
 let rock = document.getElementById("rockbutton");
 let paper = document.getElementById("paperbutton");
 let scissors = document.getElementById("scissorsbutton");
 
-rock.addEventListener("click", () => gameFlow("rock"));
-paper.addEventListener("click", () => gameFlow("paper"));
-scissors.addEventListener("click", () => gameFlow("scissors"));
+rock.addEventListener("click", () => {
+    gameFlow("rock"); 
+    clicked(rock);
+    attackPose();
+});
+paper.addEventListener("click", () => {
+    gameFlow("paper");
+    clicked(paper);
+    attackPose();
+});
+scissors.addEventListener("click", () => {
+    gameFlow("scissors");
+    clicked(scissors);
+    attackPose();
+});
 
 let playerScore = 0;
 let computerScore = 0;
@@ -59,4 +77,23 @@ function gameFlow(playerChoice) {
         else if(playerScore == computerScore)
         alert("IT'S A TIE");
     }
+}
+
+function clicked(clickedClass) {
+    clickedClass.classList.add("clicked");
+    setTimeout(() => clickedClass.classList.remove("clicked"), 5000);
+}
+
+function attackPose() {
+  let boyAttack = document.createElement('img');
+  boyAttack.src = './images/boyattack.png';
+  let robotAttack = document.createElement('img');
+  robotAttack.src = './images/robotattack.png';
+  boy.replaceChild(boyAttack, boyImage);
+  robot.replaceChild(robotAttack, robotImage);
+
+  setTimeout(() => {
+      boy.replaceChild(boyImage, boyAttack);
+      robot.replaceChild(robotImage, robotAttack);
+  }, 5000);
 }
