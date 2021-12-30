357413ee014c02b7185ebcc88409eb2a2c6319f1
mohanamisra <iammohanamisra@gmail.com>
2021-12-30 17:44:59 +0530

    Increase game functionality and add attributes

--- game.js
+++ game.js
@@ -4,10 +4,19 @@ let boyAttack = document.getElementById("boy-attack");
 let robotAttack = document.getElementById("robot-attack");
 
 let info = document.getElementById("info");
+let result = document.getElementById("result");
 
 let rock = document.getElementById("rockbutton");
 let paper = document.getElementById("paperbutton");
 let scissors = document.getElementById("scissorsbutton");
+
+let rockComputer = document.getElementById("rock-computer");
+let paperComputer = document.getElementById("paper-computer");
+let scissorsComputer = document.getElementById("scissors-computer");
+
+let yourScore = document.getElementById("your-score");
+let roboScore = document.getElementById("computer-score");
+    
 
 rock.addEventListener("click", () => {
     gameFlow("rock"); 
@@ -30,20 +39,37 @@ let computerScore = 0;
 
 function gamePlay(playerChoice) {
     let computerChoice = computerGenerate();
-    
+    if(computerChoice == "rock") {
+        rockComputer.classList.add("clicked");
+        setTimeout(() => {
+            rockComputer.classList.remove("clicked");
+        }, 1000);
+    }
+    else if(computerChoice == "paper") {
+        paperComputer.classList.add("clicked");
+        setTimeout(() => {
+            paperComputer.classList.remove("clicked");
+        }, 1000);
+    }
+    else if(computerChoice == "scissors") {
+        scissorsComputer.classList.add("clicked");
+        setTimeout(() => {
+                scissorsComputer.classList.remove("clicked");
+            }, 1000);
+    }
+
     if(playerChoice == computerChoice) {
-        console.log("Computer chose = " + computerChoice);
-        info.textContent = "Meh...";
+        info.textContent = "Robo chose " + computerChoice + " too!";
     }
     else if((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")) {
-        console.log("Computer chose = " + computerChoice);
-        info.textContent = "Yay!";
+        info.textContent = "Yay! Robo chose " + computerChoice;
         playerScore++;
+        yourScore.textContent = "You : " + playerScore;
     }
     else if((playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "paper" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "rock")) {
-        console.log("Computer chose = " + computerChoice);
-        info.textContent = "Uh oh!";
+        info.textContent = "Uh oh... Robo chose " + computerChoice;
         computerScore++;
+        roboScore.textContent = "Robo : " + computerScore;
     }
 }
 
@@ -67,22 +93,25 @@ function computerGenerate() {
 }
 
 function gameFlow(playerChoice) {
-    if(playerScore < 5 && computerScore < 5) {
-        gamePlay(playerChoice);
+    if(playerScore == 4 || computerScore == 4) {
+        if(computerScore<playerScore) {
+        yourScore.textContent = "You : 5";
+        result.textContent = "YOU WON THE GAME!";
+        }
+        else if(playerScore<computerScore) {
+        roboScore.textContent = "Robo : 5";
+        result.textContent = "You lost the game...";
+        }
+        //gameOver();
     }
     else{
-        if(computerScore<playerScore)
-        info.textContent = "YOU WON THE GAME!"
-        else if(playerScore<computerScore)
-        info.textContent = "You lost the game...";
-        else if(playerScore == computerScore)
-        info.textContent = "It's a tie!";
+        gamePlay(playerChoice);
     }
 }
 
 function clicked(clickedClass) {
     clickedClass.classList.add("clicked");
-    setTimeout(() => clickedClass.classList.remove("clicked"), 5000);
+    setTimeout(() => clickedClass.classList.remove("clicked"), 1000);
 }
 
 function attackPose() {
@@ -95,5 +124,10 @@ function attackPose() {
         boyAttack.classList.remove("make-visible");
         robot.classList.remove("make-invisible");
         robotAttack.classList.remove("make-visible");
-    }, 5000);
+    }, 2000);
 }
+
+// function gameOver() {
+//     playerScore = 0;
+//     computerScore = 0;
+// }
